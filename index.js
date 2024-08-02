c180a884656560dc40026ef0f82a276325e8139a
prafull raj <99156529+PrafullRaj@users.noreply.github.com>
2024-08-02 21:24:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(cors());
 
 const fullName = "prafull_raj";
 const dob = "03112002";
